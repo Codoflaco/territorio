c77143fa69826dab0d7634a6ab6f0cd7a82c5d98
Codoflaco <129247284+Codoflaco@users.noreply.github.com>
2025-02-15 22:55:12 -0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mapFrame = document.getElementById("mapFrame");
-    mapFrame.src = "https://maps.app.goo.gl/RyeWsgNTiws77LjJ7";
-});
