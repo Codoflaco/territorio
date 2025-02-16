344db0cfd4b3ebdca6ef43c6d3a8d701181a23ba
Codoflaco <129247284+Codoflaco@users.noreply.github.com>
2025-02-15 22:48:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mapFrame = document.getElementById("mapFrame");
+    mapFrame.src = "https://maps.app.goo.gl/RyeWsgNTiws77LjJ7";
+});
